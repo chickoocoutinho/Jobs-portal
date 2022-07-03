ac387125e2c0543fdb96f38aa336bc494e3f7237
chickoocoutinho <benito.coutinho@gmail.com>
2022-07-03 17:09:10 +0530

    set up theme and scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+
 import Details from "./components/Details/Details";
 import List from "./components/List/List";
+
+import "./App.scss";
 
 function App() {
 	return (
---
+++ src/theme/theme.js
@@ -0,0 +1,9 @@
+export const colors = {
+	primary: "#6297e5",
+	secondary: "#60616a",
+	secondaryLight: "#575c6a",
+	grey: "#d3d7e7",
+	greyLight: "#f0f3fb",
+};
+
+// export const font
